f9c710d3562f24353f696d801890675afc08caf2
bmncare <islamiyahn150@gmail.com>
2025-09-05 16:15:05 +0700

    Update bmncare.js

--- js/bmncare.js
+++ js/bmncare.js
@@ -1,8 +1,8 @@
 const bmncare = {
   care10: "bmncarekeuangan",
-  care29: "bmncarebmn",
-  care38: "bmncareinventaris",
-  care47: "bmncarekeuangan2",
-  care56: "bmncarebmn2",
-  care65: "bmncareinventaris2",
+  care20: "bmncarebmn",
+  care30: "bmncareinventaris",
+  care40: "bmncarekeuangan2",
+  care50: "bmncarebmn2",
+  care60: "bmncareinventaris2",
 };
