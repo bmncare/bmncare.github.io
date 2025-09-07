c1c34c62dd895707395df46fa541088243c09008
bmncare <islamiyahn150@gmail.com>
2025-09-07 15:35:26 +0700

    Update bmncare.js

--- js/bmncare.js
+++ js/bmncare.js
@@ -1,6 +1,6 @@
 const bmncare = {
   care10: "bmncarepusatdatadansisteminformasi",
-  care20: "bmncarebmn",
+  care20: "bmncarebiroumum",
   care30: "bmncareinventaris",
   care50: "bmncarebmn2",
   care60: "bmncareinventaris2",
